6e8dfb525a65e9883b8fc9f3bd858c6cb9beb401
lethier96 <dthiernoali@gmail.com>
2025-04-04 22:14:06 +0200

    Mise en place du test des endpoints

--- README.md
+++ README.md
@@ -39,21 +39,10 @@ Récupère toutes les prédictions présentes dans la base de données.
     3. GET /version
 Retourne la version actuelle du logiciel déployé.
     - Code HTTP 200 : Retourne la version de l'API sous forme de chaîne de caractères.
-    
+
 # Déploiement de l’API météo avec Docker
-    -   Construction de l’image Docker
-
-Assurez-vous d’être à la racine du projet (projet_final_thierno/), puis exécutez : docker-compose build.
-
+    -   Construction de l’image Docker : Assurez-vous d’être à la racine du projet (projet_final_thierno/), puis exécutez : docker-compose build.
 Cela construit une image Docker contenant : Python 3.10, FastAPI, Uvicorn, Pandas, PyYAML et le fichier ts_meteo.db monté comme volume, le modèle et les fichiers de configuration.
-
     -   Lancement du conteneur : docker-compose up -d
     -   Arrêt : docker-compose down
-    -   Tester les endpoints : 
-
-Liste des prédictions
-GET http://localhost:8000/predictions
-
-Prédictions par date :
-GET http://localhost:8000/date/2025-03-25
-(Heures valides : 00:00, 03:00, ..., 21:00)
+    -   Tester les endpoints
---
+++ api/test_api.py
@@ -0,0 +1,34 @@
+import pytest
+from fastapi.testclient import TestClient
+from api import app
+
+client = TestClient(app)
+
+def test_get_version():
+    """Teste le endpoint GET /version."""
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert "version" in response.json()
+    assert response.json()["version"] == "1.0.0"
+
+def test_get_prediction_by_date_valid():
+    """Teste le endpoint GET /date/{date} pour une date valide."""
+    date = "2025-03-25 00:00"
+    response = client.get(f"/date/{date}")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
+
+def test_get_prediction_by_date_invalid():
+    """Teste le endpoint GET /date/{date} pour une date invalide."""
+    date = "2025-03-26"
+    response = client.get(f"/date/{date}X")
+    assert response.status_code == 404
+    assert "Prédiction non trouvée" in response.json()["detail"]
+
+def test_get_all_predictions():
+    """Teste le endpoint GET /predictions."""
+    response = client.get("/predictions")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
